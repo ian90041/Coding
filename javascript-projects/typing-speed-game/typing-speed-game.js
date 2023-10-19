f51858ee056c2d0ba19b39268227696f74bad6ce
Ian <ian90041@gmail.com>
2023-10-19 16:56:41 +0800

    Setting up the game

---
+++ javascript-projects/typing-speed-game/typing-speed-game.js
@@ -0,0 +1,38 @@
+//WordLIst
+const words =[
+  "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+  "kiwi", "lemon", "mango", "orange", "papaya", "quince", "raspberry", "strawberry",
+  "tangerine", "watermelon"
+];
+
+let wordIndex = 0;
+let score = 0;
+let time = 60;
+
+// Function to display a new word
+function displayWord() {
+  const wordDisplay = document.getElementById('word-display');
+  wordDisplay.textContent = words[wordIndex];
+}
+
+displayWord();
+
+//This listener will call a function to check the input.
+const textInput = document.getElementById('text-input');
+
+textInput.addEventListener('input', checkInput); //Passing the  checkInput function, so don't have to type checkInput().
+
+function checkInput() {
+  const userInput = textInput.value;
+  const expectedText = words[wordIndex];
+
+  //Compare user input to the expected text.
+  if (userInput === expectedText) {
+    score++;
+    wordIndex++; // Move to the next word
+    textInput.value = ''; // Clear the input field
+    displayWord(); // Display the next word
+    const scoreDisplay = document.getElementById('score-value');
+    scoreDisplay.textContent = score;
+  }
+}
