a6e4ea50ab17d51c3abb595e12121e0e7f26d5b0
Ian <ian90041@gmail.com>
2023-11-09 16:36:04 +0800

    win condition

--- javascript-projects/slot-machine/scripts.js
+++ javascript-projects/slot-machine/scripts.js
@@ -1,6 +1,10 @@
 const reels = document.querySelectorAll('.slot');
 const icons = document.querySelectorAll('.icon');
 
+const moneyElement = document.getElementById('money');
+let money = 100;
+
+
 const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', spinReels);
 
@@ -18,15 +22,27 @@ function spinReels() {
     for (let reel of reels) {
       reel.classList.remove('spinning');
     }
+    checkWin();
   }, 2000); // Change the delay here to adjust the spinning duration
-
-  checkWin();
+ 
 }
-
 
 function checkWin() {
-  
+  const iconValue1 = document.getElementById('icon1').textContent;
+  const iconValue2 = document.getElementById('icon2').textContent;
+  const iconValue3 = document.getElementById('icon3').textContent;
+
+  let threeMatching = iconValue1 === iconValue2 && iconValue2 === iconValue3;
+  let twoMatching = (iconValue1 === iconValue2 && iconValue2 !== iconValue3) || 
+  (iconValue1 !== iconValue2 && iconValue2 === iconValue3 || 
+  iconValue1 === iconValue3 && iconValue1 !== iconValue2) && !threeMatching;
+
+  if (threeMatching) {
+    money += 100; //Three matching icons
+  } else if (twoMatching) {
+    money += 10; //Two matching icons
+  } else {
+    money -= 10; //No matching icons
+  }
+  moneyElement.textContent = `${money}`;
 }
-
-
-
